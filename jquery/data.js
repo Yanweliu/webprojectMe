df844611c20ddf41e38132916a724d55404cf45c
Yanweliu <edwardyanwe@gmail.com>
2019-10-01 18:53:24 +0800

    同步代码到github

---
+++ jquery/data.js
@@ -0,0 +1,94 @@
+var data = [
+
+           {
+            'name' :  '前端' ,
+             'list' :  [
+            {'title':'前端工程师',
+             'company' : 'dklfklll',
+             'number' : '123-13-13-13-',
+             'location' : '北京',
+             'experience' : '不限',
+             'education' : '本科',
+             'wage' : '面议',
+             'data' : '2019-9-23',
+            },
+            {
+             'title':'网络前端页面设计',
+             'company' : 'dklfklll',
+             'number' : '123-13-13-13-',
+             'location' : '杭州',
+             'experience' : '不限',
+             'education' : '本科',
+             'wage' : '面议',
+             'data' : '2019-9-23',
+            },
+            {
+             'title':'高级web前端工程师',
+             'company' : 'dklfklll',
+             'number' : '123-13-13-13-',
+             'location' : '杭州',
+             'experience' : '不限',
+             'education' : '本科',
+             'wage' : '面议',
+             'data' : '2019-9-23',
+            },
+            {
+            'title':'网站前端设计',
+            'company' : 'dklfklll',
+            'number' : '123-13-13-13-',
+            'location' : '杭州',
+            'experience' : '不限',
+            'education' : '本科',
+            'wage' : '面议',
+            'data' : '2019-9-23',
+             }
+        ]
+    },
+
+    {
+        name : '后端',
+        list : [
+            {
+                'title':'网站前端高级设计',
+                'company' : 'dklfklll',
+                'number' : '123-13-13-13-',
+                'location' : '杭州',
+                'experience' : '不限',
+                'education' : '本科',
+                'wage' : '面议',
+                'data' : '2019-9-23',
+            },
+            {
+                'title':'前端设计',
+                'company' : 'dklfklll',
+                'number' : '123-13-13-13-',
+                'location' : '杭州',
+                'experience' : '不限',
+                'education' : '本科',
+                'wage' : '面议',
+                'data' : '2019-9-23',
+            },
+            {
+                'title':'前端设计',
+                'company' : 'dklfklll',
+                'number' : '123-13-13-13-',
+                'location' : '杭州',
+                'experience' : '不限',
+                'education' : '本科',
+                'wage' : '面议', 
+                'data' : '2019-9-23',
+
+            },
+            {
+                'title':'php高级工程师',
+                'company' : 'dklfklll',
+                'number' : '123-13-13-13-',
+                'location' : '杭州',
+                'experience' : '不限',
+                'education' : '本科',
+                'wage' : '面议', 
+                'data' : '2019-9-23',
+            }
+        ]
+    }
+];
